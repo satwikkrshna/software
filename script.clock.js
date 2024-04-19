4fa917a391cfe0e12793b22f9ef357aa657498b5
Satwik Krishna <135621291+satwikkrshna@users.noreply.github.com>
2024-04-19 18:24:36 +0530

    Add files via upload

---
+++ script.clock.js
@@ -0,0 +1,34 @@
+setInterval(() => {
+    d = new Date();
+    htime = d.getHours();
+    mtime = d.getMinutes();
+    stime = d.getSeconds();
+    hrotation = 30 * htime + mtime / 2;
+    mrotation = 6 * mtime;
+    srotation = 6 * stime;
+
+    hour.style.transform = `rotate(${hrotation}deg)`;
+    minute.style.transform = `rotate(${mrotation}deg)`;
+    second.style.transform = `rotate(${srotation}deg)`;
+
+    let hor = document.getElementById('hr');
+    let min = document.getElementById('min');
+    let sec = document.getElementById('sec');
+    let ampm = document.getElementById('ampm');
+
+    let h = new Date().getHours();
+    let m = new Date().getMinutes();
+    let s = new Date().getSeconds();
+    let am = h >= 12 ? "PM" : "AM";
+    if (h > 12) {
+        h = h - 12;
+    }
+    h = (h < 10) ? "0" + h : h;
+    m = (m < 10) ? "0" + m : m;
+    s = (s < 10) ? "0" + s : s;
+
+    hr.innerHTML = h;
+    min.innerHTML = m;
+    sec.innerHTML = s;
+    ampm.innerHTML = am;
+}, 1000);
